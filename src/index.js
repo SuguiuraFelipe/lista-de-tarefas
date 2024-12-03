49441b95722057b608500544a8c2faffb173913d
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2024-12-03 17:59:03 +0000

    Criação dos diretórios da aplicação e desenvolvimento do arquivo index.js (servidor)

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('node:path')
+const router = require('./routes')
+
+const app = express()
+
+// Configuração EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Configuração arquivos estáticos
+app.use(express.static('public'))
+
+// configuração para ler os dados da requisição
+app.use(express.urlencoded({extended: true}))
+
+// Rotas da aplicação
+app.use(router)
+
+const PORT = process.env.PORT || 3000 // Variável de ambiente
+app.listen(PORT, () => console.log(`Servidor iniciado \nRodando em http://localhost:${PORT}/`))
